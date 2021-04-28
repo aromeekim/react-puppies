756da9fd3b4a3b4d41f6b846c780a0e2835610e7
Aromee Kim <aromeekim@aromees-mbp.home>
2021-04-27 23:33:10 -0700

    MERN-Stack Infrastructure

---
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as userService from '../../utilities/users-service';
+
+export default function OrderHistoryPage() {
+    async function handleCheckToken() {
+        const expDate = await userService.checkToken();
+        console.log(new Date(expDate));
+    }
+    return (
+    <>
+    <h1>Order History Page</h1>
+    <button onClick={handleCheckToken}>Check When My Login Expires</button>
+    </>
+    )
+}
